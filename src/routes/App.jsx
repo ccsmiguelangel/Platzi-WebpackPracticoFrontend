d904f485b6f7e78e484c436d3a3f62555256a5d9
ccsmiguelangel <miguelangel1074.gc@gmail.com>
2022-01-26 16:39:18 -0400

    :art: Añadido Add Context Provider en componentes
    Se ha dado el valor de initial state a todos los componentes del entorno

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 import Home from '../containers/Home';
 import Checkout from '../containers/Checkout';
 import Layout from '../components/Layout';
 import NotFound from '../containers/NotFound';
 import useInitialState from '../hooks/useInitialState';
-
+import AppContext from '../context/AppContext';
 const App = () => {
   const initialState = useInitialState();
   const isEmpty = Object.keys(initialState.products).length;
@@ -13,15 +14,17 @@ const App = () => {
   return(
   <>
     {isEmpty > 0 ? (
-      <Router>
-        <Layout>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/checkout" element={<Checkout />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </Router>
+      <AppContext.Provider value={initialState}>
+        <Router>
+          <Layout>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/checkout" element={<Checkout />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
+        </Router>
+      </AppContext.Provider>
       ) : <h1>Loading ...</h1>
     }
   </>
