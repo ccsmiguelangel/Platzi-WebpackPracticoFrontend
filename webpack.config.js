a9cad5837e4165234296494bb898d9b251511154
ccsmiguelangel <miguelangel1074.gc@gmail.com>
2022-01-26 15:29:22 -0400

    :wrench: Añadido archivo webpack de desarrollo y optimizaciones para el entorno de producción
    Se añadieron dependencia de desarrollo y de producción para optimizar correctamente el código para estos entornos

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CSSMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -53,4 +56,11 @@ module.exports = {
       filename: 'assets/[name].css',
     }),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CSSMinimizerPlugin(),
+      new TerserPlugin(),
+    ]
+  },
 };
