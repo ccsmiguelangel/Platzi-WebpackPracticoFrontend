dbffa70e500af552cd1234df10c2fa418bc4638a
ccsmiguelangel <miguelangel1074.gc@gmail.com>
2022-01-29 14:41:07 -0400

    :art: Configuración de Lazy Loading en archivo App.js
    Agregado componente suspense para realizar el lazy loading en proyecto

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
+import React, { Suspense } from 'react';
 
 import Home from '@containers/Home';
 import Checkout from '@containers/Checkout';
@@ -8,6 +9,9 @@ import Layout from '@components/Layout';
 import NotFound from '@containers/NotFound';
 import useInitialState from '@hooks/useInitialState';
 import AppContext from '@context/AppContext';
+
+const AsyncCheckoutContainer = React.lazy(() =>import('@containers/Checkout'));
+
 const App = () => {
   const initialState = useInitialState();
   const isEmpty = Object.keys(initialState.products).length;
@@ -15,18 +19,20 @@ const App = () => {
   return(
   <>
     {isEmpty > 0 ? (
-      <AppContext.Provider value={initialState}>
-        <Router>
-          <Layout>
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route exact path="/checkout" element={<Checkout />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Layout>
-        </Router>
-      </AppContext.Provider>
-      ) : <h1>Loading ...</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <AppContext.Provider value={initialState}>
+          <Router>
+            <Layout>
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/checkout" element={<Checkout />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Layout>
+          </Router>
+        </AppContext.Provider>
+      </Suspense>
+    ) : <h1>Loading ...</h1>
     }
   </>
   )
